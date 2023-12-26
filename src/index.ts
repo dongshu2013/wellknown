339752efb0c355b96c19c6345d6e7b901755a874
dongshu2013 <dongs2011@gmail.com>
2023-12-26 11:32:22 -0800

    add 404 page

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,18 @@ import { getPubkey } from "./ns";
 import { HexlinkError } from "./types";
 
 const app: Application = express();
-const port = process.env.PORT || 8000;
+const port = Number(process.env.PORT) || 8000;
 app.use(cors);
 app.use(express.json());
 
 app.get("/.well-known/nostr.json", async (req, res) => {
+  if (!req.query.name || typeof req.query.name !== "string") {
+    res.status(400).json({ message: "name is required" });
+    return;
+  }
+
+  const name = req.query.name;
   try {
-    const name = req.query.name as string;
     const pubkey = await getPubkey(name);
     if (pubkey) {
       res.status(200).json({ names: { [name]: pubkey } });
@@ -27,6 +32,10 @@ app.get("/.well-known/nostr.json", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
+app.get('*', function(_req, res){
+  res.status(404).send('Not Found');
+});
+
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is Fire at http://localhost:${port}`);
 });
